e110784c471adecb9c076ca458382856bcaa780d
PranjalPandey <pranjalpandeyshine@gmail.com>
2022-07-10 18:19:02 +0530

    Time: 112 ms (16.78%), Space: 14 MB (27.18%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,6 +1,9 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         c = dict(Counter(words))
-        hp = [(val,key) for key,val in c.items()]
-        hp.sort(key=lambda k: (-k[0], k[1]))
-        return [item[1] for item in hp][:k]
+        # hp = [(val,key) for key,val in c.items()]
+        # hp.sort(key=lambda k: (-k[0], k[1]))
+        # return [item[1] for item in hp][:k]
+        hp = [[-val,key] for key,val in c.items()]
+        heapq.heapify(hp)
+        return ([heapq.heappop(hp)[1] for _ in range(k)])
