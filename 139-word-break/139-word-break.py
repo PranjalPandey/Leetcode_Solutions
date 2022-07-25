44cdc6e9c9e33292c2cb40b071c748feff2e2513
PranjalPandey <pranjalpandeyshine@gmail.com>
2022-07-25 14:01:57 +0530

    Time: 51 ms (70.84%), Space: 14 MB (44.29%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,10 +1,13 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         possible = [False]*(len(s)+1)
+        se=set()
+        for i in wordDict:
+            se.add(i)
         possible[0] = True
         for i in range(len(s)):
             for j in range(i,len(s)):
-                if possible[i] and s[i:j+1] in wordDict :
+                if possible[i] and s[i:j+1] in se :
                     possible[j+1]=True
         return possible[-1]
                 
