6934a2afb85c0293ee2264b9b780d6a09b910e62
PranjalPandey <pranjalpandeyshine@gmail.com>
2022-07-26 23:45:32 +0530

    Time: 1287 ms (6.72%), Space: 25.5 MB (89.60%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,13 +1,15 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        max_so_far = nums[0]
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         max_ending_here = 0
-        for i in range(0,len(nums)):
+        max_so_far = -10**9
+        for i in range(len(nums)):
             max_ending_here += nums[i]
-            if max_ending_here< nums[i]:
+            if max_ending_here<nums[i]:
                 max_ending_here = nums[i]
-            if max_so_far < max_ending_here:
-                max_so_far =  max_ending_here
+            if max_so_far<max_ending_here:
+                max_so_far = max_ending_here
         return max_so_far
-                
-        
