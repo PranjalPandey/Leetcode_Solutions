c4dbe1280e358e50c01326161856b78cc622291e
PranjalPandey <pranjalpandeyshine@gmail.com>
2022-07-28 00:13:28 +0530

    Time: 78 ms (5.16%), Space: 13.9 MB (63.55%) - LeetHub

---
+++ 796-rotate-string/796-rotate-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        count = len(s)
+        if len(s)!=len(goal):
+            return False
+        while count:
+            if s==goal:
+                return True
+            s=s[1:]+s[0]
+            count-=1
+        return False
