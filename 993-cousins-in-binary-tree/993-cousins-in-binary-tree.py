89608fc5bcee37f9d182c8d2ace429e125bde518
PranjalPandey <pranjalpandeyshine@gmail.com>
2022-07-27 11:10:41 +0530

    Time: 62 ms (21.77%), Space: 13.9 MB (90.64%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
@@ -8,15 +8,34 @@ class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         
         res =[]
-        def dfs(node,parent,depth):
-            if node is None:
-                return
+#         def dfs(node,parent,depth):
+#             if node is None:
+#                 return
+#             if node.val==x or node.val==y:
+#                 res.append((parent,depth))
+                
+#             dfs(node.left,node,depth+1)
+#             dfs(node.right,node,depth+1)
+            
+#         dfs(root,None,0)
+#         node_x,node_y=res
+#         return node_x[0]!=node_y[0] and node_x[1]==node_y[1]
+    
+    
+    
+        q = deque()
+        q.append((root,None,0))
+        
+        while q:
+            node,parent,depth = q.popleft()
+            
             if node.val==x or node.val==y:
                 res.append((parent,depth))
+            if node.left:
+                q.append((node.left,node,depth+1))
+            if node.right:
+                q.append((node.right,node,depth+1))
                 
-            dfs(node.left,node,depth+1)
-            dfs(node.right,node,depth+1)
-            
-        dfs(root,None,0)
         node_x,node_y=res
         return node_x[0]!=node_y[0] and node_x[1]==node_y[1]
+                
