b714a62c708b41ed760fc91725b9c259e5736e9d
PranjalPandey <pranjalpandeyshine@gmail.com>
2022-08-23 17:06:46 +0530

    Time: 62 ms (43.99%), Space: 14.2 MB (69.02%) - LeetHub

---
+++ 274-h-index/274-h-index.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        h_count = [0 for i in range(len(citations)+1)]
+        h_index = 0
+        for c in citations:
+            h_count[min(c,len(citations))]+=1
+        for n in range(len(citations),-1,-1):
+            h_index +=h_count[n]
+            if h_index>= n:
+                return n
+        
